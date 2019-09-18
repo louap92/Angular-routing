c4d19de1da834bdb502edf3fb050503deafb3204
Pinhas <louis-alexandre.pinhas@devinci.fr>
2019-09-18 18:56:10 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { TermsComponent } from './terms/terms.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { TermsComponent } from './terms/terms.component';
     HomeComponent,
     AboutComponent,
     PrivacyComponent,
-    TermsComponent
+    TermsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
